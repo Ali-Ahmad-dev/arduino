e23afd5ceb6411ebd1227b3074173df8a6a585b4
AliADev <a4aliahmad123@gmail.com>
2022-09-07 13:54:47 +0500

    title modifeid

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Push Notifications'),
+        title: Text('Push Notification Title'),
       ),
       body: Center(
         child: Column(
